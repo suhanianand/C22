56dfe7c139706b430fd54921bcacba6e404f234d
suhanianand <66681604+suhanianand@users.noreply.github.com>
2020-08-27 13:31:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function setup(){
         isStatic: true
     }
     var ball_options ={
-        restitution: 0.7
+        restitution: 0.6
     }
 
     ground = Bodies.rectangle(200,390,200,20,ground_options);
